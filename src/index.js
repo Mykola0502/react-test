948c848359ce6d8df42dbce6fa22b8fb239e1a9d
Mykola Saliuk <haker0502@ukr.net>
2023-01-06 22:55:43 +0200

    додає приклади 2 модуля 2 лекції

--- src/index.js
+++ src/index.js
@@ -323,29 +323,475 @@ import ReactDOM from 'react-dom';
 /**
  *    Kонтрольовані елементи
  */
-class App extends Component {
+// class App extends Component {
+//   state = {
+//     inputValue: '',
+//   };
+
+//   handleChange = evt => {
+//     this.setState({ inputValue: evt.target.value });
+//   };
+
+//   render() {
+//     const { inputValue } = this.state;
+//     console.log(inputValue);
+//     return (
+//       <>
+//         <input type="text" value={inputValue} onChange={this.handleChange} />
+//         <span>{inputValue}</span>
+//       </>
+//     );
+//   }
+// }
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+/**
+ *            Складні форми
+ */
+// class SignUpForm extends Component {
+//   state = {
+//     login: '',
+//   };
+
+//   // Відповідає за оновлення стану
+//   handleChange = e => {
+//     this.setState({ login: e.target.value });
+//   };
+
+//   // Викликається під час відправлення форми
+//   handleSubmit = evt => {
+//     evt.preventDefault();
+//     console.log(`Signed up as: ${this.state.login}`);
+
+//     // Проп, який передається формі для виклику під час сабміту
+//     this.props.onSubmit({ ...this.state });
+//   };
+
+//   render() {
+//     const { login } = this.state;
+
+//     return (
+//       <form onSubmit={this.handleSubmit}>
+//         <label>
+//           Name
+//           <input
+//             type="text"
+//             placeholder="Enter login"
+//             value={login}
+//             onChange={this.handleChange}
+//           />
+//         </label>
+
+//         <button type="submit">Sign up as {login}</button>
+//       </form>
+//     );
+//   }
+// }
+
+// ReactDOM.render(
+//   <SignUpForm onSubmit={values => console.log(values)} />,
+//   document.getElementById('root')
+// );
+
+/**
+ *  Додамо поля для email і password, а заразом використаємо дуже корисний патерн для callback-функції, що передається в onChange.
+ */
+// Виносимо об'єкт із примітивами в константу, щоб було зручно скидати.
+// Не можна використовувати, якщо в якійсь властивості стану зберігається складний тип.
+// const INITIAL_STATE = {
+//   login: '',
+//   email: '',
+//   password: '',
+// };
+
+// class SignUpForm extends React.Component {
+//   state = { ...INITIAL_STATE };
+
+//   // Для всіх інпутів створюємо один обробник
+//   // Розрізняти інпути будемо за атрибутом name
+//   handleChange = evt => {
+//     const { name, value } = evt.target;
+//     this.setState({ [name]: value });
+//   };
+
+//   handleSubmit = evt => {
+//     evt.preventDefault();
+//     const { login, email, password } = this.state;
+//     console.log(`Login: ${login}, Email: ${email}, Password: ${password}`);
+//     this.props.onSubmit({ ...this.state });
+//     this.reset();
+//   };
+
+//   reset = () => {
+//     this.setState({ ...INITIAL_STATE });
+//   };
+
+//   render() {
+//     const { login, email, password } = this.state;
+
+//     return (
+//       <form onSubmit={this.handleSubmit}>
+//         <label>
+//           Name
+//           <input
+//             type="text"
+//             placeholder="Enter login"
+//             name="login"
+//             value={login}
+//             onChange={this.handleChange}
+//           />
+//         </label>
+//         <label>
+//           Email
+//           <input
+//             type="email"
+//             placeholder="Enter email"
+//             name="email"
+//             value={email}
+//             onChange={this.handleChange}
+//           />
+//         </label>
+//         <label>
+//           Password
+//           <input
+//             type="password"
+//             placeholder="Enter password"
+//             name="password"
+//             value={password}
+//             onChange={this.handleChange}
+//           />
+//         </label>
+
+//         <button type="submit">Sign up as {login}</button>
+//       </form>
+//     );
+//   }
+// }
+
+// ReactDOM.render(
+//   <SignUpForm onSubmit={values => console.log(values)} />,
+//   document.getElementById('root')
+// );
+
+/**
+ *            Чекбокси
+ *
+ *
+ */
+// const INITIAL_STATE = {
+//   login: '',
+//   email: '',
+//   password: '',
+//   agreed: false,
+// };
+
+// class SignUpForm extends React.Component {
+//   state = {
+//     ...INITIAL_STATE,
+//   };
+
+//   handleChange = evt => {
+//     const { name, value, type, checked } = evt.target;
+//     // Якщо тип елемента – checkbox, беремо значення checked,
+//     // в іншому випадку – value
+//     this.setState({ [name]: type === 'checkbox' ? checked : value });
+//   };
+
+//   handleSubmit = e => {
+//     e.preventDefault();
+//     const { login, email, password, agreed } = this.state;
+//     console.log(
+//       `Login: ${login}, Email: ${email}, Password: ${password}, Agreed: ${agreed}`
+//     );
+
+//     /* ... */
+//   };
+
+//   render() {
+//     const { login, email, password, agreed } = this.state;
+
+//     return (
+//       <form onSubmit={this.handleSubmit}>
+//         <label>
+//           Name
+//           <input
+//             type="text"
+//             placeholder="Enter login"
+//             name="login"
+//             value={login}
+//             onChange={this.handleChange}
+//           />
+//         </label>
+//         <label>
+//           Email
+//           <input
+//             type="email"
+//             placeholder="Enter email"
+//             name="email"
+//             value={email}
+//             onChange={this.handleChange}
+//           />
+//         </label>
+//         <label>
+//           Password
+//           <input
+//             type="password"
+//             placeholder="Enter password"
+//             name="password"
+//             value={password}
+//             onChange={this.handleChange}
+//           />
+//         </label>
+//         <label>
+//           Agree to terms
+//           <input
+//             type="checkbox"
+//             checked={agreed}
+//             onChange={this.handleChange}
+//           />
+//         </label>
+
+//         <button type="submit" disabled={!agreed}>
+//           Sign up as {login}
+//         </button>
+//       </form>
+//     );
+//   }
+// }
+// ReactDOM.render(
+//   <SignUpForm onSubmit={values => console.log(values)} />,
+//   document.getElementById('root')
+// );
+
+/**
+ *           Радіокнопки
+ *
+ */
+// const Gender = {
+//   MALE: 'male',
+//   FEMALE: 'female',
+// };
+
+// const INITIAL_STATE = {
+//   login: '',
+//   email: '',
+//   password: '',
+//   agreed: false,
+//   gender: null,
+// };
+
+// class SignUpForm extends React.Component {
+//   state = {
+//     ...INITIAL_STATE,
+//   };
+
+//   handleChange = evt => {
+//     const { name, value, type, checked } = evt.target;
+//     // Якщо тип елемента – checkbox, беремо значення checked,
+//     // в іншому випадку – value
+//     this.setState({ [name]: type === 'checkbox' ? checked : value });
+//   };
+
+//   handleSubmit = e => {
+//     e.preventDefault();
+//     const { login, email, password, agreed } = this.state;
+//     console.log(
+//       `Login: ${login}, Email: ${email}, Password: ${password}, Agreed: ${agreed}`
+//     );
+
+//     /* ... */
+//   };
+
+//   render() {
+//     const { login, email, password, agreed, gender } = this.state;
+
+//     return (
+//       <form onSubmit={this.handleSubmit}>
+//         <label>
+//           Name
+//           <input
+//             type="text"
+//             placeholder="Enter login"
+//             name="login"
+//             value={login}
+//             onChange={this.handleChange}
+//           />
+//         </label>
+//         <label>
+//           Email
+//           <input
+//             type="email"
+//             placeholder="Enter email"
+//             name="email"
+//             value={email}
+//             onChange={this.handleChange}
+//           />
+//         </label>
+//         <label>
+//           Password
+//           <input
+//             type="password"
+//             placeholder="Enter password"
+//             name="password"
+//             value={password}
+//             onChange={this.handleChange}
+//           />
+//         </label>
+
+//         <section>
+//           <h2>Choose your gender</h2>
+//           <label>
+//             Male
+//             <input
+//               type="radio"
+//               checked={gender === Gender.MALE}
+//               name="gender"
+//               value={Gender.MALE}
+//               onChange={this.handleChange}
+//             />
+//           </label>
+//           <label>
+//             Female
+//             <input
+//               type="radio"
+//               checked={gender === Gender.FEMALE}
+//               name="gender"
+//               value={Gender.FEMALE}
+//               onChange={this.handleChange}
+//             />
+//           </label>
+//         </section>
+
+//         <button type="submit" disabled={!agreed}>
+//           Sign up as {login}
+//         </button>
+//       </form>
+//     );
+//   }
+// }
+// ReactDOM.render(
+//   <SignUpForm onSubmit={values => console.log(values)} />,
+//   document.getElementById('root')
+// );
+
+/**
+ *               Селект
+ */
+const Gender = {
+  MALE: 'male',
+  FEMALE: 'female',
+};
+const INITIAL_STATE = {
+  login: '',
+  email: '',
+  password: '',
+  agreed: false,
+  gender: null,
+  age: '',
+};
+
+class SignUpForm extends React.Component {
   state = {
-    inputValue: '',
+    ...INITIAL_STATE,
   };
 
   handleChange = evt => {
-    this.setState({ inputValue: evt.target.value });
+    const { name, value, type, checked } = evt.target;
+    // Якщо тип елемента – checkbox, беремо значення checked,
+    // в іншому випадку – value
+    this.setState({ [name]: type === 'checkbox' ? checked : value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const { login, email, password, agreed } = this.state;
+    console.log(
+      `Login: ${login}, Email: ${email}, Password: ${password}, Agreed: ${agreed}`
+    );
+
+    /* ... */
   };
 
   render() {
-    const { inputValue } = this.state;
-    console.log(inputValue);
+    const { login, email, password, agreed, gender, age } = this.state;
+
     return (
-      <>
-        <input type="text" value={inputValue} onChange={this.handleChange} />
-        <span>{inputValue}</span>
-      </>
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name
+          <input
+            type="text"
+            placeholder="Enter login"
+            name="login"
+            value={login}
+            onChange={this.handleChange}
+          />
+        </label>
+        <label>
+          Email
+          <input
+            type="email"
+            placeholder="Enter email"
+            name="email"
+            value={email}
+            onChange={this.handleChange}
+          />
+        </label>
+        <label>
+          Password
+          <input
+            type="password"
+            placeholder="Enter password"
+            name="password"
+            value={password}
+            onChange={this.handleChange}
+          />
+        </label>
+
+        <section>
+          <h2>Choose your gender</h2>
+          <label>
+            Male
+            <input
+              type="radio"
+              checked={gender === Gender.MALE}
+              name="gender"
+              value={Gender.MALE}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            Female
+            <input
+              type="radio"
+              checked={gender === Gender.FEMALE}
+              name="gender"
+              value={Gender.FEMALE}
+              onChange={this.handleChange}
+            />
+          </label>
+        </section>
+        <label>
+          Choose your age
+          <select name="age" value={age} onChange={this.handleChange}>
+            <option value="" disabled>
+              ...
+            </option>
+            <option value="18-25">18-25</option>
+            <option value="26-35">26-35</option>
+            <option value="36+">36+</option>
+          </select>
+        </label>
+
+        <button type="submit" disabled={!agreed}>
+          Sign up as {login}
+        </button>
+      </form>
     );
   }
 }
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-/**
- *
- */
+ReactDOM.render(
+  <SignUpForm onSubmit={values => console.log(values)} />,
+  document.getElementById('root')
+);
